f454b858554008793de36de377e566a355223e64
JevH08 <56478191+JevH08@users.noreply.github.com>
2020-05-30 13:39:49 +0700

    ralat report v1

--- Yinyinpedia/CrystalSubReportPenjualanSeller.cs
+++ Yinyinpedia/CrystalSubReportPenjualanSeller.cs
@@ -103,22 +103,6 @@ namespace Yinyinpedia {
                 return this.ReportDefinition.Sections[6];
             }
         }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.Shared.IParameterField Parameter_getTanggalStart {
-            get {
-                return this.DataDefinition.ParameterFields[0];
-            }
-        }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.Shared.IParameterField Parameter_getTanggalEnd {
-            get {
-                return this.DataDefinition.ParameterFields[1];
-            }
-        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
