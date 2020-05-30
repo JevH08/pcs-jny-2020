f6c70889f13d4c64636c836f4f7f1e9c22f695ab
JevH08 <56478191+JevH08@users.noreply.github.com>
2020-05-30 14:51:33 +0700

    crystal ralat v3

--- Yinyinpedia/CrystalPengiriman.cs
+++ Yinyinpedia/CrystalPengiriman.cs
@@ -111,14 +111,6 @@ namespace Yinyinpedia {
                 return this.DataDefinition.ParameterFields[0];
             }
         }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.Shared.IParameterField Parameter_CrystalSubReportPengirimanrpt_getMonth {
-            get {
-                return this.DataDefinition.ParameterFields[1];
-            }
-        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
