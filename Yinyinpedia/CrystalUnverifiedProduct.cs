b014613551903c98ce835b3e353e7e166c4c748e
JevH08 <56478191+JevH08@users.noreply.github.com>
2020-06-06 23:34:39 +0700

    jevon sblm merge yinyin

--- Yinyinpedia/CrystalUnverifiedProduct.cs
+++ Yinyinpedia/CrystalUnverifiedProduct.cs
@@ -103,14 +103,6 @@ namespace Yinyinpedia {
                 return this.ReportDefinition.Sections[6];
             }
         }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.Shared.IParameterField Parameter_getKategori {
-            get {
-                return this.DataDefinition.ParameterFields[0];
-            }
-        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
